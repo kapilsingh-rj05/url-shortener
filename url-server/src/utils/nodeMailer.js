7cf783247e00a440c73b38e44f7d2f4b76e7b62d
kapilsingh-rj05 <kapilkawai1504@gmail.com>
2026-05-27 11:58:20 +0530

    fix: update nodemailer transport configuration to use Gmail service

--- url-server/src/utils/nodeMailer.js
+++ url-server/src/utils/nodeMailer.js
@@ -3,9 +3,7 @@ import nodemailer from "nodemailer"
 const sendEmail = async(to, subject, text, html) => {
     try {
         const transporter = nodemailer.createTransport({
-            service:"smtp.gmail.com",
-            port:465,
-            secure:true,
+            service:"gmail",
             auth:{
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
